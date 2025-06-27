9ed4bc0bdb9adc4b4b6b358b3c588c4c5946ce3c
maxins1211 <lamvo.general@gmail.com>
2025-06-26 21:16:14 -0400

    Add signup route and navigation link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 const App = () => {
+  const padding = {
+    padding: 5,
+  }
   return (
     <Router>
+      <div>
+        <Link style={padding} to="/">
+          Home
+        </Link>
+        <Link style={padding} to="/login">
+          Login
+        </Link>
+        <Link style={padding} to="/signup">
+          SignUp
+        </Link>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
