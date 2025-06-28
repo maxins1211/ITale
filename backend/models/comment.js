5e2a5213cc788380b30649b1fced5114f5b96433
maxins1211 <lamvo.general@gmail.com>
2025-06-27 20:05:16 -0400

    Add Comment Model

---
+++ backend/models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const commentSchema = mongoose.Schema({
+    content: { type: String, required: true },
+    blog: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Blog',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+commentSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment
