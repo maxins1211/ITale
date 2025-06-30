bc25d06f743160244b58d3de012cc6aa1ae8e3a6
maxins1211 <lamvo.general@gmail.com>
2025-06-29 23:28:32 -0400

    Create route for get all comments and add a comment to a specific blog

--- backend/controllers/blogs.route.js
+++ backend/controllers/blogs.route.js
@@ -1,5 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const middleware = require('../utils/middleware')
 
 blogsRouter.get("/", async (request, response) => {
@@ -62,4 +63,42 @@ blogsRouter.put("/:id", async (request, response) => {
     const updatedBlog = await Blog.findByIdAndUpdate(id, { likes, author, url, title }, { new: true }).populate("user", { username: 1, name: 1, id: 1 })
     response.status(200).json(updatedBlog)
 })
+
+// Get all comments for a specific blog
+blogsRouter.get("/:id/comments", async (request, response) => {
+    const blogId = request.params.id
+    const comments = await Comment.find({ blog: blogId })
+        .populate("user", { username: 1, name: 1, id: 1 })
+        .sort({ createdAt: -1 })
+    response.json(comments)
+})
+
+// Add a comment to a specific blog
+blogsRouter.post("/:id/comments", middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
+    const { content } = request.body
+    const user = request.user
+    const blogId = request.params.id
+
+    const blog = await Blog.findById(blogId)
+    if (!blog) {
+        return response.status(404).json({ error: 'Blog not found' })
+    }
+
+    if (!content || content.trim().length === 0) {
+        return response.status(400).json({ error: 'Comment content is required' })
+    }
+
+    const comment = new Comment({
+        content: content.trim(),
+        blog: blogId,
+        user: user._id
+    })
+
+    const savedComment = await comment.save()
+    const populatedComment = await Comment.findById(savedComment._id)
+        .populate("user", { username: 1, name: 1, id: 1 })
+
+    response.status(201).json(populatedComment)
+})
+
 module.exports = blogsRouter;
