799983574edba6db42c9042ac6be1edb23567e46
maxins1211 <lamvo.general@gmail.com>
2025-06-30 23:55:01 -0400

    Update env variable for imagekit

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,4 +3,16 @@ const PORT = process.env.PORT
 const MONGODB_URI = process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
-module.exports = { PORT, MONGODB_URI }
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT || 3003}`
+const IMAGEKIT_PUBLIC_KEY = process.env.IMAGEKIT_PUBLIC_KEY
+const IMAGEKIT_PRIVATE_KEY = process.env.IMAGEKIT_PRIVATE_KEY
+const IMAGEKIT_URL_ENDPOINT = process.env.IMAGEKIT_URL_ENDPOINT
+
+module.exports = {
+    PORT,
+    MONGODB_URI,
+    BASE_URL,
+    IMAGEKIT_PUBLIC_KEY,
+    IMAGEKIT_PRIVATE_KEY,
+    IMAGEKIT_URL_ENDPOINT
+}
