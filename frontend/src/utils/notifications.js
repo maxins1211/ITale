d2128c0ee467f14d5a13951dc764b8036d3dc475
maxins1211 <lamvo.general@gmail.com>
2025-07-01 20:07:26 -0400

    Add utils for toastify content render

---
+++ frontend/src/utils/notifications.js
@@ -0,0 +1,45 @@
+import { toast } from 'react-toastify'
+
+export const showSuccessNotification = (message) => {
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    })
+}
+
+export const showErrorNotification = (message) => {
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    })
+}
+
+export const showInfoNotification = (message) => {
+    toast.info(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    })
+}
+
+export const showWarningNotification = (message) => {
+    toast.warning(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    })
+}
