3c6a5f91b9cf4f244793dd374f6ef1ebc896ce3c
maxins1211 <lamvo.general@gmail.com>
2025-07-01 20:12:05 -0400

    Remove Notification in Login Component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,8 @@
 import LoginForm from '../components/LoginForm'
-import Notification from '../components/Notification'
 
 const Login = () => {
   return (
     <div>
-      <Notification />
-      <h2>Log in to application</h2>
       <LoginForm />
     </div>
   )
