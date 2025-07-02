acf63f757a615f9d6362e3cbcee7bf5dafa3d318
maxins1211 <lamvo.general@gmail.com>
2025-07-01 20:09:48 -0400

    Implement ThemeProvider and ToastContainer for App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { loginUser } from './reducers/userReducer'
 import blogService from './services/blogs'
+import { ThemeProvider, useTheme } from './components/theme-provider'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -10,8 +14,10 @@ import SingleBlog from './pages/SingleBlog'
 import CreateBlog from './pages/CreateBlog'
 import EditBlog from './pages/EditBlog'
 
-const App = () => {
+// Component to access theme inside the provider
+const AppContent = () => {
   const dispatch = useDispatch()
+  const { theme } = useTheme()
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
@@ -22,34 +28,43 @@ const App = () => {
     }
   }, [dispatch])
 
-  const padding = {
-    padding: 5,
-  }
   return (
     <Router>
-      <div>
-        <Link style={padding} to="/">
-          Home
-        </Link>
-        <Link style={padding} to="/login">
-          Login
-        </Link>
-        <Link style={padding} to="/signup">
-          SignUp
-        </Link>
-        <Link style={padding} to="/create-blog">
-          Create Blog
-        </Link>
+      <div className="min-h-screen bg-background text-foreground transition-colors">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/blogs/:id" element={<SingleBlog />} />
+            <Route path="/create-blog" element={<CreateBlog />} />
+            <Route path="/blogs/:id/edit" element={<EditBlog />} />
+          </Routes>
+        </main>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme={theme === 'dark' ? 'dark' : 'light'}
+          className="mt-16"
+        />
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/blogs/:id" element={<SingleBlog />} />
-        <Route path="/create-blog" element={<CreateBlog />} />
-        <Route path="/blogs/:id/edit" element={<EditBlog />} />
-      </Routes>
     </Router>
+  )
+}
+
+const App = () => {
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <AppContent />
+    </ThemeProvider>
   )
 }
 
